df2ede4985ee2e3c3429343f0859c7398f7b8dae
Pablo Sousa <pablofroz@hotmail.com.br>
2022-12-23 14:50:13 -0300

    Update product.service.ts

--- test/src/main/webapp/angular-frontend/src/app/services/product.service.ts
+++ test/src/main/webapp/angular-frontend/src/app/services/product.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
+  private headerOptions: any = {
+    observe: 'response',
+    headers: new HttpHeaders({
+      'Authorization': 'Basic dWRhY2l0eTpwYXNzd29yZA=='
+    })
+};
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get("http://localhost:8080/api/products");
+  getProducts(): any {
+    return this.http.get("http://localhost:8080/api/products", this.headerOptions);
   }
 
   getProduct(id: number){
-    return this.http.get("http://localhost:8080/api/product/" + id);
+    return this.http.get("http://localhost:8080/api/product/" + id, this.headerOptions);
   }
 }
